1d42013361b002504bb219c894ee1eea71264ee0
KLZ-0 <adrian@kalazi.com>
2020-01-10 15:05:51 +0100

    Changed conversion from fscanf to strol + patched the case if a file ptr would be NULL

--- main.c
+++ main.c
@@ -1,36 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <sys/utsname.h>
 #include <time.h>
+#include <limits.h>
 
 #define KERNEL_ENDCHAR '-'
+#define FLOATING_POINT_CHAR '.'
 #define DATE_BUFFER_SIZE 30
 
-int getIntFromFile(char *fname) {
-	int num;
+int findCharPositionInStream(FILE *stream, char ch) {
+	int pos = 0;
+	int tmp;
+	while (tmp = fgetc(stream), tmp != EOF && tmp != ch && tmp != '\n') {
+		pos++;
+	}
+	rewind(stream);
+	return pos;
+}
+
+long getIntFromFile(char *fname) {
 	FILE *file;
 	file = fopen(fname, "r");
-	if (!fscanf(file, "%d", &num)) { // NOLINT(cert-err34-c)
+	if (file == NULL) {
+		return 0;
+	}
+	int len = findCharPositionInStream(file, FLOATING_POINT_CHAR)+1;
+	char *tmp = calloc(len, sizeof(char));
+	if (tmp == NULL) {
+		fclose(file);
+		return 0;
+	}
+	if(fgets(tmp, len, file) == NULL) {
+		free(tmp);
 		fclose(file);
 		return 0;
 	}
 	fclose(file);
+
+	char *endptr = NULL;
+	long num = strtol(tmp, &endptr, 0);
+	if (*endptr || num == LONG_MAX || num == LONG_MIN) {
+		free(tmp);
+		return 0;
+	}
+	free(tmp);
 	return num;
 }
 
 void printUptime() {
-	int seconds = getIntFromFile("/proc/uptime");
-	int d = seconds/86400;
-	int h = seconds/3600%24;
-	int m = seconds/60%60;
+	long seconds = getIntFromFile("/proc/uptime");
+	long d = seconds/86400;
+	long h = seconds/3600%24;
+	long m = seconds/60%60;
 	if (d) {
-		printf("%d days, ", d);
+		printf("%li days, ", d);
 	}
 	if (h) {
-		printf("%d:%s%d", h, (m < 10) ? "0" : "", m);
+		printf("%li:%s%li", h, (m < 10) ? "0" : "", m);
 	}
 	else {
-		printf("%d min", m);
+		printf("%li min", m);
 	}
 }
 
@@ -43,14 +73,17 @@ void printKernelVersion() {
 }
 
 void printBatteryPercentage() {
-	int batnow = getIntFromFile("/sys/class/power_supply/BAT0/charge_now");
-	int batfull = getIntFromFile("/sys/class/power_supply/BAT0/charge_full");
-	printf("%d%%", (int)round(batnow/(double)batfull*100));
+	long batnow = getIntFromFile("/sys/class/power_supply/BAT0/charge_now");
+	long batfull = getIntFromFile("/sys/class/power_supply/BAT0/charge_full");
+	printf("%d%%", (int)round((double)batnow/(double)batfull*100));
 }
 
 void printBatteryStatus() {
 	FILE *batstat_f;
 	batstat_f = fopen("/sys/class/power_supply/BAT0/status", "r");
+	if (batstat_f == NULL) {
+		return;
+	}
 	int ch;
 	while (ch = fgetc(batstat_f), ch != EOF && ch != '\n') {
 		printf("%c", ch);
