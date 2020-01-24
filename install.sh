c0d60159ad8f8bd5093ed3278f9614e93fc4b927
KLZ-0 <adrian@kalazi.com>
2020-01-24 02:03:35 +0100

    Fixed paths in install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 # Installation directory can be overridden, defaults to ~/.local/bin
 export INSTALL_DIR=~/.local/bin
 
+tmp=$( realpath "$0"  )
+PROJECT_PATH=$(dirname "$tmp")
+
+cd $PROJECT_PATH
 cmake -B cmake-build-release -D CMAKE_BUILD_TYPE=Release
 cd cmake-build-release
 mkdir -p $INSTALL_DIR
